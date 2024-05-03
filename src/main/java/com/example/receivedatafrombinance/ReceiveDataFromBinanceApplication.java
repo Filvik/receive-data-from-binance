23fdc0a108145fc3713fac650272dc4c1dd386e1
Filvik <filvik@bk.ru>
2024-05-03 19:57:06 +0300

    init

---
+++ src/main/java/com/example/receivedatafrombinance/ReceiveDataFromBinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.receivedatafrombinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiveDataFromBinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiveDataFromBinanceApplication.class, args);
+	}
+
+}
